19d3c79ffbd834996532c035e62ce9048720eb71
Janescience <janescience.top@gmail.com>
2022-10-03 04:40:50 +0000

    Deploying to gh-pages from @ Janescience/cow-app@34c98071b3c525b104ff5597efbe353365416484 🚀

---
+++ assets/auth-header.2a074e3c.js
@@ -0,0 +1,1 @@
+import{y as r}from"./index.8283c5b3.js";function s(){let e=r().currentUser;return e&&e.accessToken?{Authorization:e.accessToken}:{}}export{s as a};
--- assets/auth-header.6b837446.js
+++
@@ -1,1 +0,0 @@
-import{y as r}from"./index.cad1d48f.js";function s(){let e=r().currentUser;return e&&e.accessToken?{Authorization:e.accessToken}:{}}export{s as a};
