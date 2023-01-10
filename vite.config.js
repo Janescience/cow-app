1bf5c8f7d1c6d49cfaec17391e9a70bc4bab2812
janwit.si <janwit.si@ocean.co.th>
2023-01-10 20:33:46 +0700

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,41 @@ export default defineConfig((command,mode) =>{
       vue(),
       VitePWA({ 
         registerType: 'autoUpdate' ,
+        injectRegister: 'auto',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
+          cleanupOutdatedCaches: false
+        },
+        manifest : {
+          name : 'Dairy Farm',
+          short_name : 'daifar',
+          description : 'Dairy farm management application',
+          theme_color : '#1F2937',
+          background_color : '#1F2937',
+          display : 'standalone',
+          icons : [
+            {
+              src : 'img/icons/icon-192x192.png',
+              sizes : '192x192',
+              type : 'image/png'
+            },
+            {
+              src : 'img/icons/icon-256x256.png',
+              sizes : '256x256',
+              type : 'image/png'
+            },
+            {
+              src : 'img/icons/icon-384x384.png',
+              sizes : '384x384',
+              type : 'image/png'
+            },
+            {
+              src : 'img/icons/icon-512x512.png',
+              sizes : '512x512',
+              type : 'image/png'
+            },
+          ]
+        },
         devOptions: {
           enabled: true
         }
