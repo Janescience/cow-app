43cd8ba28f9c0991d74a102be7457f6bce761d7a
janescience <janescience.top@gmail.com>
2023-02-16 16:49:53 +0700

    remove vite pwa

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig , loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vitejs.dev/config/
@@ -12,43 +12,43 @@ export default defineConfig((command,mode) =>{
     base: '/',
     plugins: [
       vue(),
-      VitePWA({ 
-        registerType: 'autoUpdate' ,
-        injectRegister: 'auto',
-        manifest : {
-          name : 'Dairy Farm',
-          short_name : 'Dairy Farm',
-          description : 'Dairy farm management application',
-          theme_color : '#1F2937',
-          background_color : '#1F2937',
-          display : 'standalone',
-          icons : [
-            {
-              src : 'image/icons/icon-192x192.png',
-              sizes : '192x192',
-              type : 'image/png'
-            },
-            {
-              src : 'image/icons/icon-256x256.png',
-              sizes : '256x256',
-              type : 'image/png'
-            },
-            {
-              src : 'image/icons/icon-384x384.png',
-              sizes : '384x384',
-              type : 'image/png'
-            },
-            {
-              src : 'image/icons/icon-512x512.png',
-              sizes : '512x512',
-              type : 'image/png'
-            },
-          ]
-        },
-        devOptions: {
-          enabled: true
-        }
-      })
+      // VitePWA({ 
+      //   registerType: 'autoUpdate' ,
+      //   injectRegister: 'auto',
+      //   manifest : {
+      //     name : 'Dairy Farm',
+      //     short_name : 'Dairy Farm',
+      //     description : 'Dairy farm management application',
+      //     theme_color : '#1F2937',
+      //     background_color : '#1F2937',
+      //     display : 'standalone',
+      //     icons : [
+      //       {
+      //         src : 'image/icons/icon-192x192.png',
+      //         sizes : '192x192',
+      //         type : 'image/png'
+      //       },
+      //       {
+      //         src : 'image/icons/icon-256x256.png',
+      //         sizes : '256x256',
+      //         type : 'image/png'
+      //       },
+      //       {
+      //         src : 'image/icons/icon-384x384.png',
+      //         sizes : '384x384',
+      //         type : 'image/png'
+      //       },
+      //       {
+      //         src : 'image/icons/icon-512x512.png',
+      //         sizes : '512x512',
+      //         type : 'image/png'
+      //       },
+      //     ]
+      //   },
+      //   devOptions: {
+      //     enabled: true
+      //   }
+      // })
     ],
     resolve: {
       alias: {
