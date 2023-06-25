bf19207f2506dd9f2ffd2e10f2f4ff25a32593b1
janescience <janescience.top@gmail.com>
2023-06-25 17:31:31 +0700

    when click outside menu on mbile is closed menu

--- src/components/SectionMain.vue
+++ src/components/SectionMain.vue
@@ -1,5 +1,16 @@
+<script setup>
+import { useLayoutStore } from '@/store/theme/layout.js'
+
+const layoutStore = useLayoutStore()
+
+const outsideMenuClick = () => {
+  layoutStore.isAsideMobileExpanded = false
+}
+
+
+</script>
 <template>
-  <section class="px-0 min-h-screen md:px-6 py-6 text-sm lg:text-base">
+  <section class="px-0 min-h-screen md:px-6 py-6 text-sm lg:text-base" @click="outsideMenuClick">
     <slot />
   </section>
 </template>
